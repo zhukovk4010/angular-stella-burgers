9cd12d655850ec297fb60e5ab27fc2bd694ac947
Zhukov Kirill <zhukovk4010@gmail.com>
2024-04-06 14:07:22 +0300

    Design a header for a mobile phone application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "../shared/ui/header/header.component";
+import {MainComponent} from "../pages/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
