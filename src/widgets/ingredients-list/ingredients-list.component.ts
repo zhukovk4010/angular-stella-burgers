50a7fdb92da3ba8f31e9502c3bbd9f4f356c18fd
Zhukov Kirill <zhukovk4010@gmail.com>
2024-04-07 22:42:26 +0300

    Add ingredients list widget

---
+++ src/widgets/ingredients-list/ingredients-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredients-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './ingredients-list.component.html',
+  styleUrl: './ingredients-list.component.scss'
+})
+export class IngredientsListComponent {
+
+}
